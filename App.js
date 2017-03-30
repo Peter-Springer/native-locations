24232d1c27cc452cc12eaef700829db788a1b37c
Peter Springer <peterspringer829@gmail.com>
2017-03-30 17:13:32 -0600

    initial commit and console log geolocation

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition((position) => console.log(position));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>YOOOOOOO</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
